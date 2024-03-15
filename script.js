300f6e8c82206399394001e795b60359327a4862
Abhishekgth <128219073+Abhishekgth@users.noreply.github.com>
2024-03-15 17:04:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  const targetElement = document.getElementById('level');
+  let domLevel = 0;
+
+  if (targetElement) {
+    let parent = targetElement.parentElement;
+
+    while (parent) {
+      domLevel++;
+      parent = parent.parentElement;
+    }
+
+    
+    domLevel++;
+
+    alert(`The level of the element is: ${domLevel}`);
+  } else {
+    alert('Element with id "level" not found.');
+  }
+});
